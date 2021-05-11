16e973214ab8e213054efc38e42e13d2ff3a5ecb
= <=>
2021-05-12 01:06:50 +0400

    script is ready for being used

--- install.cmd
+++ install.cmd
@@ -1,6 +1,9 @@
 mkdir Debug
 ECHO Докачиваю SFML библиотеку...
-cd sfml/tomove
+curl https://www.sfml-dev.org/files/SFML-2.5.1-windows-vc15-32-bit.zip -o tomove.zip
+tar -xf tomove.zip
+del tomove.zip
+cd SFML-2.5.1/bin
 ECHO Перенос openal32.dll
 move openal32.dll ../../Debug
 ECHO Перенос sfml-graphics-2.dll
@@ -15,6 +18,6 @@ ECHO Перенос sfml-window-2.dll
 move sfml-window-2.dll ../../Debug
 ECHO Перенос sfml-window-d-2.dll
 move sfml-window-d-2.dll ../../Debug
-cd ..
-rmdir tomove
+cd ../..
+ren SFML-2.5.1 sfml-res
 ECHO Готово!
